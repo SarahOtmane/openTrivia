e4edab64bbe45c6747f49e8d76260aafe74b8034
SarahOtmane <sarahotmane02@gmail.com>
2024-05-02 09:30:09 +0200

    style de base

---
+++ style.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native';import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    
+    title: {
+        fontSize: 60,
+        fontWeight: 'bold',
+        marginVertical: 20
+    },
+})
