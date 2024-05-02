147b1e9fb629f418a94abc05f4cdd010ce94841f
SarahOtmane <sarahotmane02@gmail.com>
2024-05-02 09:33:45 +0200

    creer le composant navigator

--- App.js
+++ App.js
@@ -1,10 +1,43 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 
-
+const Tab = createBottomTabNavigator();
 
 const App = () =>{
     return(
-        <></>
+        <NavigationContainer>
+            <Tab.Navigator 
+                screenOptions={({route}) => ({
+                    tabBarIcon:({focused, color, size}) =>{
+                        let iconName;
+
+                        if(route.name === 'Home'){
+                            iconName = focused ? 'home' : 'home-outline';
+                        }else if(route.name === 'About'){
+                            iconName = focused ? 'information-circle' : 'information-circle-outline';
+                        }else if(route.name === 'Third'){
+                            iconName = focused ? 'search' : 'search-outline';
+                        }
+                            return <Ionicons name={iconName} size={size} color={color} />
+                        },
+                        tabBarActiveTintColor : '#ba0d7b',
+                        tabBarInactiveTintColor: '#333333'
+                })}
+            >
+                <Tab.Screen 
+                    name="Home" 
+                    component={Home} 
+                    options={{
+                        title:"Home", 
+                        headerStyle: {
+                            backgroundColor: '#6d75b5'
+                        }
+                    }} 
+                /> 
+            </Tab.Navigator>
+        </NavigationContainer>
     )
 }
 
