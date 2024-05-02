e4055dc379bded9948b4586add5718ea9e3204c7
SarahOtmane <sarahotmane02@gmail.com>
2024-05-02 09:59:54 +0200

    definir les regles du jeux dans le composant Home.js

--- components/Home.js
+++ components/Home.js
@@ -7,10 +7,26 @@ import style from '../style.js';
 const Home = () =>{
     return(
         <View style={style.container}>
-            <Text style={style.title}>Bienvenue</Text>
-            <View style={style.plage_container} >
-                <Text>Rendez vous à la plage</Text>
-                <Image style={style.plage_image} source={require('../assets/plage.jpeg')} />
+            <Text style={style.title}>Quizz Trivia</Text>
+            
+            <View style={style.container}>
+                <Text>Règles du jeux : </Text>
+                <Text>
+                    1. Toutes les questions de ce quizz seront à choix multiples
+                </Text>
+                <Text>
+                    2. Il y'a trois modes de difficultés (facile/moyen/difficile).
+                    Par défaut, aucun mode n'est choisis donc vous aurez des questions des trois modes. Vous pouvez le modifier à tout moment dans les réglages.
+                </Text>
+                <Text>
+                    3. Il y'a plusieurs catégories différentes. Par défaut, vous avez des questions de toutes les catégories mais vous pouvez en séléctionner une dans les réglages.
+                </Text>
+                <Text>
+                    4. À chaque question vous aurez 4 réponse, seule une est correct.
+                </Text>
+                <Text>
+                    5. Votre score sera visible en haut de la page.
+                </Text>
             </View>
         </View>
     )
