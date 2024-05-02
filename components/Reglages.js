b3e7870dae9b3965e46b25eb52bf4caeaa70033f
SarahOtmane <sarahotmane02@gmail.com>
2024-05-02 11:46:01 +0200

    liste deroulante pour le choix de la categrie, difficulté

--- components/Reglages.js
+++ components/Reglages.js
@@ -1,8 +1,55 @@
+import React, { useState } from 'react';
+import { Text, View, Button} from 'react-native';
 
+import {Picker} from "@react-native-picker/picker";
 
+import style from '../style.js';
 
 const Reglages = () =>{
+    const [Categorie , setCategorie]  = useState("-1");
+    const [Difficulte , setDifficulte]  = useState("-1");
 
+    return(
+        <View style={style.container}>
+            <Text style={style.title}>Modifier vos réglages</Text>
+
+            <View style={style.container}>
+                <Text style={style.sousTitre} >Choisissez une catégorie.</Text>
+                <Picker
+                  selectedValue={Categorie}
+                  mode={"dialog"}
+                  onValueChange={(catego) => setCategorie(catego)}
+                  style={style.picker}
+                >
+                    <Picker.Item label="Any Category" value="-1" />
+                    <Picker.Item label="General Knowledge" value="9" />
+                    <Picker.Item label="Entertainment: Books" value="10" />
+                    <Picker.Item label="Entertainment: Music" value="12" />
+                    <Picker.Item label="Entertainment: Film" value="11" />
+                    <Picker.Item label="Entertainment: Musicals & Theatres" value="13" />
+                    <Picker.Item label="Entertainment: Television" value="14" />
+                    <Picker.Item label="Entertainment: Video Games" value="15" />
+                    <Picker.Item label="Entertainment: Board Games" value="16" />
+                    <Picker.Item label="Science & Nature" value="17" />
+                    <Picker.Item label="Science: Computers" value="18" />
+                    <Picker.Item label="Science: Mathematics" value="19" />
+                </Picker>
+            
+                <Text style={[style.sousTitre, style.label]} >Choisissez la difféculté.</Text>
+                <Picker
+                  selectedValue={Difficulte}
+                  mode={"dialog"}
+                  onValueChange={(diff) => setDifficulte(diff)}
+                  style={style.picker}
+                >
+                    <Picker.Item label="Any Category" value="-1" />
+                    <Picker.Item label="Easy" value="easy" />
+                    <Picker.Item label="Medium" value="medium" />
+                    <Picker.Item label="Hard" value="hard" />
+                </Picker>
+            </View>
+        </View>
+    )
 }
 
 export default Reglages;
