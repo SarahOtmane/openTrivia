e24d4c12dd89ec9882ce3730a0a159c923770331
SarahOtmane <sarahotmane02@gmail.com>
2024-05-03 09:41:08 +0200

    fautes d orthographe

--- components/Home.js
+++ components/Home.js
@@ -15,17 +15,17 @@ const Home = () =>{
             <View style={[style.container, style.regle]} >
                 <Text style={style.sousTitre}>Règles du jeu : </Text>
                 <Text style={style.regle}>
-                    1. Toutes les questions de ce quizz seront à choix multiples.
+                    1. Toutes les questions de ce quiz seront à choix multiples.
                 </Text>
                 <Text style={style.regle}>
-                    2. Il y'a trois modes de difficultés (facile/moyen/difficile).
-                    Par défaut, aucun mode n'est choisis donc vous aurez des questions des trois modes. Vous pouvez le modifier à tout moment dans les réglages.
+                    2.  Il y a trois modes de difficultés (facile/moyen/difficile).
+                    Par défaut, aucun mode n'est choisi, donc vous aurez des questions des trois modes. Vous pouvez le modifier à tout moment dans les réglages.
                 </Text>
                 <Text style={style.regle}>
-                    3. Il y'a plusieurs catégories différentes. Par défaut, vous avez des questions de toutes les catégories mais vous pouvez en séléctionner une dans les réglages.
+                    3. Il y a plusieurs catégories différentes. Par défaut, vous avez des questions de toutes les catégories mais vous pouvez en sélectionner une dans les réglages.
                 </Text>
                 <Text style={style.regle}>
-                    4. Vous avez 15 questions aléatoires. À chaque question vous aurez 4 réponses, seule une est correct.
+                    4. Vous avez 15 questions aléatoires. À chaque question, vous aurez 4 réponses, seule une est correcte.
                 </Text>
                 <Text style={style.regle}>
                     5. Votre score sera visible en haut de la page.
